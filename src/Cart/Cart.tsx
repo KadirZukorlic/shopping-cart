66ceb6525bcd1179510c80f9d97cb37e1039d334
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-04-26 17:49:42 +0200

    calculate total

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -11,18 +11,22 @@ interface Props {
 }
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) =>  acc + item.amount * item.price, 0)
+
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
       {cartItems.length === 0 ? <p>No items in cart.</p> : null}
       {cartItems.map((item) => (
-        <CartItem 
+        <CartItem
           key={item.id}
           item={item}
           addToCart={addToCart}
           removeFromCart={removeFromCart}
         />
       ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 };
